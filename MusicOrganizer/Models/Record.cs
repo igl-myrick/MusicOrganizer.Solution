b9b56bb2da0a75ab8915b12dd6fd5abfecb374d9
igl-myrick <igl.myrick@gmail.com>
2024-10-01 15:23:01 -0700

    add id property

--- MusicOrganizer/Models/Record.cs
+++ MusicOrganizer/Models/Record.cs
@@ -5,12 +5,14 @@ namespace MusicOrganizer.Models
   public class Record
   {
     public string Title { get; set; }
+    public int Id { get; }
     private static List<Record> _instances = new List<Record> { };
 
     public Record(string title)
     {
       Title = title;
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
     public static List<Record> GetAll()
