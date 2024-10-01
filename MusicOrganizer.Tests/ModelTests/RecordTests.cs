30fb1801abcf8f210477f91f72d03044a4c47050
igl-myrick <igl.myrick@gmail.com>
2024-10-01 14:59:50 -0700

    add test for GetAll method and update past test syntax

--- MusicOrganizer.Tests/ModelTests/RecordTests.cs
+++ MusicOrganizer.Tests/ModelTests/RecordTests.cs
@@ -11,23 +11,36 @@ namespace MusicOrganizer.Tests
     [TestMethod]
     public void RecordConstructor_CreatesInstanceOfRecord_Record()
     {
-      Record newRecord = new Record();
+      Record newRecord = new Record("title");
       Assert.AreEqual(typeof(Record), newRecord.GetType());
     }
 
     public void GetTitle_GetsValueOfTitle_String()
     {
-      Record newRecord = new Record();
-      string title = newRecord.Title;
+      string title = "title";
+      Record newRecord = new Record(title);
       Assert.AreEqual(title, newRecord.Title);
     }
 
     public void SetTitle_SetsValueOfTitle_String()
     {
-      Record newRecord = new Record();
+      string title = "title";
+      Record newRecord = new Record(title);
       string newTitle = "new title";
       newRecord.Title = newTitle;
-      Assert.AreEqual(newTitle, newRecord.Title);
+      string result = newRecord.Title;
+      Assert.AreEqual(newTitle, result);
+    }
+
+    public void GetAll_ReturnsRecords_RecordList()
+    {
+      Record newRecord1 = new Record("Title 1");
+      Record newRecord2 = new Record("Title 2");
+      List<Record> newList = new List<Record> { newRecord1, newRecord2 };
+
+      List<Record> result = Record.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
