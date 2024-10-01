3c072535576dce5c2687fb5966deadf403df5dd3
igl-myrick <igl.myrick@gmail.com>
2024-10-01 15:04:29 -0700

    add GetAll method

--- MusicOrganizer/Models/Record.cs
+++ MusicOrganizer/Models/Record.cs
@@ -5,5 +5,22 @@ namespace MusicOrganizer.Models
   public class Record
   {
     public string Title { get; set; }
+    private static List<Record> _instances = new List<Record> { };
+
+    public Record(string title)
+    {
+      Title = title;
+      _instances.Add(this);
+    }
+
+    public static List<Record> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
