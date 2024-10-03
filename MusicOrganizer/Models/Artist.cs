6441461035d84a8332d6ce7964dd50434a8ef8ea
igl-myrick <igl.myrick@gmail.com>
2024-10-03 15:40:04 -0700

    add Get property and instance constructor

--- MusicOrganizer/Models/Artist.cs
+++ MusicOrganizer/Models/Artist.cs
@@ -4,6 +4,13 @@ namespace MusicOrganizer.Models
 {
   public class Artist
   {
-    
+    private static List<Artist> _instances = new List<Artist> {};
+    public string Name { get; }
+
+    public Artist(string name)
+    {
+      Name = name;
+      _instances.Add(this);
+    }
   }
 }
