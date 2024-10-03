1e60332537aa40eb2c4ef3cb03d71fdb4d381af8
igl-myrick <igl.myrick@gmail.com>
2024-10-03 15:38:43 -0700

    add test for Name property get functionality

--- MusicOrganizer.Tests/ModelTests/ArtistTests.cs
+++ MusicOrganizer.Tests/ModelTests/ArtistTests.cs
@@ -11,8 +11,17 @@ namespace MusicOrganizer.Tests
     [TestMethod]
     public void ArtistConstructor_CreatesInstanceOfArtist_Artist()
     {
-      Artist newArtist = new Artist();
+      Artist newArtist = new Artist("name");
       Assert.AreEqual(typeof(Artist), newArtist.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_GetsValueOfName_String()
+    {
+      string name = "artist name";
+      Artist newArtist = new Artist(name);
+      string result = newArtist.Name;
+      Assert.AreEqual(name, result);
     }
   }
 }
