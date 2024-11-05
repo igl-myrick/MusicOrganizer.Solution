29688900345c80da8b376cde6231091937c1adb9
igl-myrick <igl.myrick@gmail.com>
2024-11-04 16:04:12 -0800

    update Record to work with MySqlConnector

--- MusicOrganizer/Models/Record.cs
+++ MusicOrganizer/Models/Record.cs
@@ -1,33 +1,106 @@
 using System.Collections.Generic;
+using MySqlConnector;
 
 namespace MusicOrganizer.Models
 {
   public class Record
   {
     public string Title { get; set; }
-    public int Id { get; }
-    private static List<Record> _instances = new List<Record> { };
+    public int Id { get; set; }
 
-    public Record(string title)
+    public Record(string title, int id)
     {
       Title = title;
-      _instances.Add(this);
-      Id = _instances.Count;
+      Id = id;
+    }
+
+    public override bool Equals(System.Object otherRecord)
+    {
+      if (!(otherRecord is Record))
+      {
+        return false;
+      }
+      else
+      {
+        Record newRecord = (Record) otherRecord;
+        bool idEquality = (this.Id == newRecord.Id);
+        bool titleEquality = (this.Title == newRecord.Title);
+        return titleEquality;
+      }
+    }
+
+    public override int GetHashCode()
+    {
+      return Id.GetHashCode();
     }
 
     public static List<Record> GetAll()
     {
-      return _instances;
+      List<Record> allRecords = new List<Record> {};
+
+      MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+      conn.Open();
+
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand();
+      cmd.CommandText = "SELECT * FROM records;";
+
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+        int recordId = rdr.GetInt32(0);
+        string recordTitle = rdr.GetString(1);
+        Record newRecord = new Record(recordTitle, recordId);
+        allRecords.Add(newRecord);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allRecords;
     }
 
     public static void ClearAll()
     {
-      _instances.Clear();
+      MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+      conn.Open();
+
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = "DELETE FROM records;";
+      cmd.ExecuteNonQuery();
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
     }
 
     public static Record Find(int searchId)
     {
-      return _instances[searchId-1];
+      Record placeholderRecord = new Record("placeholder record");
+      return placeholderRecord;
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+      conn.Open();
+      
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = "INSERT INTO records (title) VALUES (@RecordTitle);";
+      MySqlParameter param = new MySqlParameter();
+      param.ParameterName = "@RecordTitle";
+      param.Value = this.Title;
+      cmd.Parameters.Add(param);    
+      cmd.ExecuteNonQuery();
+      Id = (int) cmd.LastInsertedId;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
     }
   }
 }
