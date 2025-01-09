57dde72c48b45df665bfdd411b0b508ed9b11acd
igl-myrick <igl.myrick@gmail.com>
2025-01-08 16:03:10 -0800

    update Artist class to work with ef core

--- MusicOrganizer/Models/Artist.cs
+++ MusicOrganizer/Models/Artist.cs
@@ -4,37 +4,8 @@ namespace MusicOrganizer.Models
 {
   public class Artist
   {
-    private static List<Artist> _instances = new List<Artist> {};
+    public int ArtistId { get; set; }
     public string Name { get; set; }
-    public int Id { get; }
-    public List<Record> Records { get; set; }
-
-    public Artist(string name)
-    {
-      Name = name;
-      _instances.Add(this);
-      Id = _instances.Count;
-      Records = new List<Record>{};
-    }
-
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-
-    public static List<Artist> GetAll()
-    {
-      return _instances;
-    }
-
-    public static Artist Find(int searchId)
-    {
-      return _instances[searchId-1];
-    }
-
-    public void AddRecord(Record record)
-    {
-      Records.Add(record);
-    }
+    public List<ArtistSong> JoinEntities { get; }
   }
 }
