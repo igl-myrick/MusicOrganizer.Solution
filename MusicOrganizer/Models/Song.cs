e5070aac0a749bf48d1a65e7fb636154a88b0a78
igl-myrick <igl.myrick@gmail.com>
2025-01-10 14:17:28 -0800

    add validation attributes

--- MusicOrganizer/Models/Song.cs
+++ MusicOrganizer/Models/Song.cs
@@ -7,8 +7,10 @@ namespace MusicOrganizer.Models
   public class Song
   {
     public int SongId { get; set; }
+    [Required(ErrorMessage = "Please enter a valid title.")]
     public string Title { get; set; }
-    public int ArtistId { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "You must add an album for this song first.")]
+    public int AlbumId { get; set; }
     public Album Album { get; set; }
     public List<ArtistSong> JoinEntities { get; }
   }
