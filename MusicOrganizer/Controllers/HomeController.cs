60bb0c3dfb9fa68eaec2ca0369277052e384d6df
igl-myrick <igl.myrick@gmail.com>
2025-01-15 16:00:06 -0800

    update Index route to include album and song lists

--- MusicOrganizer/Controllers/HomeController.cs
+++ MusicOrganizer/Controllers/HomeController.cs
@@ -1,15 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using MusicOrganizer.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MusicOrganizer.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly MusicOrganizerContext _db;
 
-    [HttpGet("/")]
-    public ActionResult Index()
+    public HomeController(MusicOrganizerContext db)
     {
-      return View();
+      _db = db;
     }
 
+    public ActionResult Index()
+    {
+      Album[] albums = _db.Albums.ToArray();
+      Song[] songs = _db.Songs.ToArray();
+      Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+      model.Add("albums", albums);
+      model.Add("songs", songs);
+      return View(model);
+    }
   }
 }
