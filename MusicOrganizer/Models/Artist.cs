d094e861e11e47672611bb21abe92c7f0a39d5e4
igl-myrick <igl.myrick@gmail.com>
2025-01-23 14:32:46 -0800

    require Name property

--- MusicOrganizer/Models/Artist.cs
+++ MusicOrganizer/Models/Artist.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MusicOrganizer.Models
 {
   public class Artist
   {
     public int ArtistId { get; set; }
+    [Required(ErrorMessage = "Please enter a valid name.")]
     public string Name { get; set; }
     public List<ArtistSong> JoinEntities { get; }
   }
