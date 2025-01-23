7f7933dff2b71dcef6b0112454825545c6db5842
igl-myrick <igl.myrick@gmail.com>
2025-01-23 14:32:35 -0800

    require Title property

--- MusicOrganizer/Models/Album.cs
+++ MusicOrganizer/Models/Album.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MusicOrganizer.Models
 {
   public class Album
   {
     public int AlbumId { get; set; }
+    [Required(ErrorMessage = "Please enter a valid title.")]
     public string Title { get; set; }
     public List<Song> Songs { get; set; }
   }
