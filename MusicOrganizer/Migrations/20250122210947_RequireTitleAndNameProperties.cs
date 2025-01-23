0efe0059a46e3cf67e613874032bea8b6927e5be
igl-myrick <igl.myrick@gmail.com>
2025-01-23 14:32:01 -0800

    require title and name properties

---
+++ MusicOrganizer/Migrations/20250122210947_RequireTitleAndNameProperties.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicOrganizer.Migrations
+{
+    public partial class RequireTitleAndNameProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "Name",
+                keyValue: null,
+                column: "Name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Artists",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "Title",
+                keyValue: null,
+                column: "Title",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Albums",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Artists",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Albums",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
